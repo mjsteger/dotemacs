4c1901b80e29571affe833dec3450e26ae5f11d3
mjsteger <mjsteger1@gmail.com>
2012-06-20 11:54:48 -0700

    Adding mozrepl for ease of javascript development

--- lisp/modes.el
+++ lisp/modes.el
@@ -1,7 +1,6 @@
 (require-all '(
 	       load-ido ; Loads ido-mode and various tweaks for it
 	       load-globals ; Load all the global always-on modes
-
 	       load-colors ; Set up  my color theme
 	       load-org ; Set up my org-mode files and tweaks
 	       load-autocomplete ; Set up autocompletion based on dabbrev
@@ -10,7 +9,7 @@
 	       load-buffer ; Load my ibuffer customizations and so forth
 	       load-languages ; Provides language tweaks for emacs.
 	       load-personal ; Loads all the stuff I've personally written
-	       load-ecb ; Tweaks ecb's semantic sources and loads it
+	       ;load-ecb ; Tweaks ecb's semantic sources and loads it
 	       load-emacs-tweaks ; Smallish tweaks to emacs
 	       load-bookmark ; Set up bookmark+ and related utilities
 	       load-markdown ; Set up special editing for markdown
@@ -20,4 +19,5 @@
 	       load-fci ; Little blue line to let you see 80 chars
 	       load-yasnippet ; Templating system in emacs!
 	       load-modeline ; Reconjigger the modeline to my liking
+	       load-moz
 	       ))
