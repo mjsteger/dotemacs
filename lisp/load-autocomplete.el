8c899a41f77099932c137104cb49587dc0289c53
mjsteger <mjsteger1@gmail.com>
2012-07-04 18:57:44 -0700

    Be able to browse autocompletes with C-{n,p}

--- lisp/load-autocomplete.el
+++ lisp/load-autocomplete.el
@@ -7,8 +7,12 @@
 (set-default 'ac-sources '(ac-source-yasnippet ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-symbols))
 
 (define-key ac-completing-map [return] 'ac-complete)
+(define-key ac-completing-map (kbd "M-o") 'ac-complete)
 
-(ac-config-default)
 (ac-flyspell-workaround)
 
 
+
+(setq ac-use-menu-map t)
+(define-key ac-menu-map "\C-n" 'ac-next)
+(define-key ac-menu-map "\C-p" 'ac-previous)
