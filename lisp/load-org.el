4954551c4df65a0db222219aa4effd7ff7d690f9
mjsteger <mjsteger1@gmail.com>
2012-07-04 18:58:26 -0700

    Major overhaul of org stuff, commenting the cruft for now, will delete if I don't have to refer back to it

--- lisp/load-org.el
+++ lisp/load-org.el
@@ -2,15 +2,15 @@
 (setq org-startup-indented t)
 (require 'org-install)
 
+(require 'org-capture)
 (defvar home (expand-file-name "~/"))
 
 (setq org-log-done t)
 
-(setq org-agenda-files (list (concat home "/org/school.org")
-                             (concat home "/org/home.org")
+(setq org-agenda-files (list (concat home "/org/home.org")
 			     (concat home "/org/gtd.org")
-			     (concat home "/org/google.org")
 			     (concat home "/org/work.org")
+			     (concat home "/org/notes.org")
 			     (concat home "/org/refile.org")
 			     ))
 
@@ -19,40 +19,42 @@
       '(
 	("t" "Todo" entry (file+headline "~/org/gtd.org" "Tasks")
 	 "* TODO %?\n  %i\n ")
-	("n" "Note" entry (file "~/org/refile.org")
-	 "* %? :NOTE:\n%U\\n  %i" :clock-in t :clock-resume t)
-	("a" "Interesting" entry (file "~/national/interesting.org")
-	 "* %? :NOTE:\n%U\\n  %i" :clock-in t :clock-resume t)
+	("n" "Note" entry (file "~/org/notes.org")
+	 "* %? :NOTE:\n%U\n  %i" :clock-in t :clock-resume t)
+	("a" "Interesting" entry (file "~/org/notes.org")
+	 "* %? :NOTE:\n%U\n  %i" :clock-in t :clock-resume t)
 	("e" "Emacs" entry (file "~/org/emacs.org")
 	 "* TODO %?\n  %i\n " )
 	("h" "Home" entry (file+headline "~/org/home.org" "Tasks")
 	 "* TODO %?\n  %i\n ")
-	("s" "School" entry (file+headline "~/org/school.org" "Tasks")
-	 "* TODO %?\n  %i\n ")
-        ("j" "Journal" entry (file+datetree "~/org/journal.org")
-	 "* %?\nEntered on %U\n  %i\n ")))
+	("j" "Journal" entry (file+datetree "~/org/journal.org")
+	 "* %?\nEntered on %U\n  %i\n ")
+	("w" "Work" entry (file+headline "~/org/work.org" "Tasks")
+	 ("* TODO %?\n %i\n"))
+	("r" "Refile" entry (file+headline "~/org/refile.org" "Tasks")
+	 ("* TODO %?\n %i\n"))))
 
-(setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
-              (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)")
-              (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
+;; (setq org-todo-keywords
+;;       (quote ((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d!/!)")
+;;               (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "|" "CANCELLED(c@/!)")
+;;               (sequence "OPEN(O!)" "|" "CLOSED(C!)"))))
 
-(setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "red" :weight bold)
-              ("NEXT" :foreground "yellow" :weight bold)
-              ("STARTED" :foreground "yellow" :weight bold)
-              ("DONE" :foreground "forest green" :weight bold)
-              ("WAITING" :foreground "orange" :weight bold)
-              ("SOMEDAY" :foreground "magenta" :weight bold)
-              ("CANCELLED" :foreground "forest green" :weight bold)
-              ("OPEN" :foreground "blue" :weight bold)
-              ("CLOSED" :foreground "forest green" :weight bold)
-              )))
+;; (setq org-todo-keyword-faces
+;;       (quote (("TODO" :foreground "red" :weight bold)
+;;               ("NEXT" :foreground "yellow" :weight bold)
+;;               ("STARTED" :foreground "yellow" :weight bold)
+;;               ("DONE" :foreground "forest green" :weight bold)
+;;               ("WAITING" :foreground "orange" :weight bold)
+;;               ("SOMEDAY" :foreground "magenta" :weight bold)
+;;               ("CANCELLED" :foreground "forest green" :weight bold)
+;;               ("OPEN" :foreground "blue" :weight bold)
+;;               ("CLOSED" :foreground "forest green" :weight bold)
+;;               )))
 
 
 ;; Custom agenda command definitions
 (setq org-agenda-custom-commands
-      (quote (("N" "Notes" tags "NOTE"
+      (quote (("n" "Notes" tags "NOTE"
                ((org-agenda-overriding-header "Notes")
                 (org-tags-match-list-sublevels t)))
               ("h" "Habits" tags-todo "STYLE=\"habit\""
@@ -106,7 +108,7 @@
                ((org-agenda-overriding-header "Stuck Projects")
                 (org-tags-match-list-sublevels 'indented)
                 (org-agenda-skip-function 'bh/skip-non-stuck-projects)))
-              ("n" "Next Tasks" tags-todo "-WAITING-CANCELLED/!NEXT|STARTED"
+              ("N" "Next Tasks" tags-todo "-WAITING-CANCELLED/!NEXT|STARTED"
                ((org-agenda-overriding-header "Next Tasks")
                 (org-agenda-skip-function 'bh/skip-projects)
                 (org-tags-match-list-sublevels t)
@@ -138,59 +140,59 @@
 
 (add-hook 'org-finalize-agenda-hook 'org-agenda-to-appt)
 
-;;; define categories that should be excluded
-(setq org-export-exclude-category (list "google" "private"))
+;; ;;; define categories that should be excluded
+;; (setq org-export-exclude-category (list "google" "private"))
 
-;;; define filter. The filter is called on each entry in the agenda.
-;;; It defines a regexp to search for two timestamps, gets the start
-;;; and end point of the entry and does a regexp search. It also
-;;; checks if the category of the entry is in an exclude list and
-;;; returns either t or nil to skip or include the entry.
+;; ;;; define filter. The filter is called on each entry in the agenda.
+;; ;;; It defines a regexp to search for two timestamps, gets the start
+;; ;;; and end point of the entry and does a regexp search. It also
+;; ;;; checks if the category of the entry is in an exclude list and
+;; ;;; returns either t or nil to skip or include the entry.
 
-(defun org-mycal-export-limit ()
-  "Limit the export to items that have a date, time and a range. Also exclude certain categories."
-  (setq org-tst-regexp "<\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\} ... [0-9]\\{2\\}:[0-9]\\{2\\}[^\r\n>]*?\
-\)>")
-  (setq org-tstr-regexp (concat org-tst-regexp "--?-?" org-tst-regexp))
-  (save-excursion
-    ; get categories
-    (setq mycategory (org-get-category))
-    ; get start and end of tree
-    (org-back-to-heading t)
-    (setq mystart    (point))
-    (org-end-of-subtree)
-    (setq myend      (point))
-    (goto-char mystart)
-    ; search for timerange
-    (setq myresult (re-search-forward org-tstr-regexp myend t))
-    ; search for categories to exclude
-    (setq mycatp (member mycategory org-export-exclude-category))
-    ; return t if ok, nil when not ok
-    (if (and myresult (not mycatp)) t nil)))
+;; (defun org-mycal-export-limit ()
+;;   "Limit the export to items that have a date, time and a range. Also exclude certain categories."
+;;   (setq org-tst-regexp "<\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\} ... [0-9]\\{2\\}:[0-9]\\{2\\}[^\r\n>]*?\
+;; \)>")
+;;   (setq org-tstr-regexp (concat org-tst-regexp "--?-?" org-tst-regexp))
+;;   (save-excursion
+;;     ; get categories
+;;     (setq mycategory (org-get-category))
+;;     ; get start and end of tree
+;;     (org-back-to-heading t)
+;;     (setq mystart    (point))
+;;     (org-end-of-subtree)
+;;     (setq myend      (point))
+;;     (goto-char mystart)
+;;     ; search for timerange
+;;     (setq myresult (re-search-forward org-tstr-regexp myend t))
+;;     ; search for categories to exclude
+;;     (setq mycatp (member mycategory org-export-exclude-category))
+;;     ; return t if ok, nil when not ok
+;;     (if (and myresult (not mycatp)) t nil)))
 
-;;; activate filter and call export function
-(defun org-mycal-export ()
-  (let ((org-icalendar-verify-function 'org-mycal-export-limit))
-    (org-export-icalendar-combine-agenda-files)))
+;; ;;; activate filter and call export function
+;; (defun org-mycal-export ()
+;;   (let ((org-icalendar-verify-function 'org-mycal-export-limit))
+;;     (org-export-icalendar-combine-agenda-files)))
 
-(run-with-idle-timer 60 t 'kill-google-if-exists)
+;; (run-with-idle-timer 60 t 'kill-google-if-exists)
 
-(defun kill-google-if-exists ()
-  (when (get-buffer "google.org")
-    (kill-buffer "google.org")))
+;; (defun kill-google-if-exists ()
+;;   (when (get-buffer "google.org")
+;;     (kill-buffer "google.org")))
 
-(defadvice org-capture-finalize
-  (before add-to-google-calendar)
-  "Add a new Google calendar entry that mirrors the diary entry just created by org-mode."
-  (let ((thing-to-do (car (split-string (cadr(split-string (car (split-string (buffer-substring-no-properties (point-min) (point-max)) "Scheduled")) "TODO")) "\n")))
-	(time-of-activity (car (split-string (cadr (split-string (cadr (split-string (buffer-substring-no-properties (point-min) (point-max)) "scheduled:")) "<")) ">"))))
-    (message (format "~/googlecl/src/google calendar add \" %s %s \" " thing-to-do time-of-activity))
-    (shell-command (format "~/googlecl/src/google calendar add \" %s %s \" " thing-to-do time-of-activity))
-    (when (get-buffer "google.org")
-      (kill-buffer "google.org"))
-    (delete-region (point-min) (point-max) )))
+;; (defadvice org-capture-finalize
+;;   (before add-to-google-calendar)
+;;   "Add a new Google calendar entry that mirrors the diary entry just created by org-mode."
+;;   (let ((thing-to-do (car (split-string (cadr(split-string (car (split-string (buffer-substring-no-properties (point-min) (point-max)) "Scheduled")) "TODO")) "\n")))
+;; 	(time-of-activity (car (split-string (cadr (split-string (cadr (split-string (buffer-substring-no-properties (point-min) (point-max)) "scheduled:")) "<")) ">"))))
+;;     (message (format "~/googlecl/src/google calendar add \" %s %s \" " thing-to-do time-of-activity))
+;;     (shell-command (format "~/googlecl/src/google calendar add \" %s %s \" " thing-to-do time-of-activity))
+;;     (when (get-buffer "google.org")
+;;       (kill-buffer "google.org"))
+;;     (delete-region (point-min) (point-max) )))
 
-(ad-activate 'org-capture-finalize)
+;; (ad-activate 'org-capture-finalize)
 
 ;; Start the weekly agenda today
 (setq org-agenda-start-on-weekday nil)
