6697a9d96187e5929b1a1ebfdc341fba1099e287
mjsteger <mjsteger1@gmail.com>
2012-07-04 18:54:07 -0700

    Added swank and slime js, added rspec mode and emacs-quickrun, and updated the .gitmodules to reflect

---
+++ site-lisp/setup-slime-js.el
@@ -0,0 +1,84 @@
+(setq slime-js-target-url "http://localhost:3000")
+(setq slime-js-connect-url "http://localhost:8009")
+(setq slime-js-starting-url "/")
+(setq slime-js-swank-command "swank-js")
+(setq slime-js-swank-args '())
+(setq slime-js-browser-command "open -a Safari")
+(setq slime-js-browser-jacked-in nil)
+
+(defun slime-js-run-swank ()
+  "Runs the swank side of the equation."
+  (interactive)
+  (unless (boundp 'slime-js-swank-buffer)
+    (setq slime-js-swank-buffer
+          (apply #'make-comint "swank-js" slime-js-swank-command nil slime-js-swank-args))))
+
+(defun slime-js-jack-in-node ()
+  "Start a swank-js server and connect to it, opening a repl."
+  (interactive)
+  (slime-js-run-swank)
+  (sleep-for 1)
+  (setq slime-protocol-version 'ignore)
+  (slime-connect "localhost" 4005))
+
+(defun slime-js-jack-in-browser ()
+  "Start a swank-js server, connect to it, open a repl, open a browser, connect to that."
+  (interactive)
+  (slime-js-jack-in-node)
+  (sleep-for 2)
+  (slime-js-set-target-url slime-js-target-url)
+  (shell-command (concat slime-js-browser-command " " slime-js-connect-url slime-js-starting-url))
+  (sleep-for 3)
+  (setq slime-remote-history nil)
+  (slime-js-sticky-select-remote (caadr (slime-eval '(js:list-remotes))))
+  (setq slime-js-browser-jacked-in t)
+  (global-set-key [f5] 'slime-js-reload))
+
+(defadvice save-buffer (after save-css-buffer activate)
+  (when (and slime-js-browser-jacked-in (eq major-mode 'css-mode))
+    (slime-js-refresh-css)))
+
+(defun js2-is-eval-friendly-node (n)
+  (or (and (js2-stmt-node-p n) (not (js2-block-node-p n)))
+      (and (js2-function-node-p n) (js2-function-node-name n))))
+
+(defun slime-js--echo-result (result &rest _)
+  (message result))
+
+(defun slime-js--replace-with-result (replacement beg end)
+  (save-excursion
+    (goto-char beg)
+    (delete-char (- end beg))
+    (insert replacement)))
+
+(defun slime-js-eval-region (beg end &optional func)
+  (lexical-let ((func (or func 'slime-js--echo-result))
+                (beg beg)
+                (end end))
+    (slime-flash-region beg end)
+    (slime-js-eval
+     (buffer-substring-no-properties beg end)
+     #'(lambda (s) (funcall func (cadr s) beg end)))))
+
+(defun slime-js-eval-statement (&optional func)
+  (let ((node (js2r--closest 'js2-is-eval-friendly-node)))
+    (slime-js-eval-region (js2-node-abs-pos node)
+                          (js2-node-abs-end node)
+                          func)))
+
+(defun slime-js-eval-current ()
+  (interactive)
+  (if (use-region-p)
+      (slime-js-eval-region (point) (mark))
+    (slime-js-eval-statement)))
+
+(defun slime-js-eval-and-replace-current ()
+  (interactive)
+  (if (use-region-p)
+      (slime-js-eval-region (point) (mark) 'slime-js--replace-with-result)
+    (slime-js-eval-statement 'slime-js--replace-with-result)))
+
+(define-key slime-js-minor-mode-map (kbd "C-x C-e") 'slime-js-eval-current)
+(define-key slime-js-minor-mode-map (kbd "C-c C-e") 'slime-js-eval-and-replace-current)
+
+(provide 'setup-slime-js)
