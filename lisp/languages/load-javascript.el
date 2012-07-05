ff31693970b6e95f27b920c73bbcaaa11d5082ab
mjsteger <mjsteger1@gmail.com>
2012-07-04 18:59:59 -0700

    Add all the slime stuff

--- lisp/languages/load-javascript.el
+++ lisp/languages/load-javascript.el
@@ -1,6 +1,21 @@
 (require 'js2-mode)
 
+(require 'slime-js)
+(require 'setup-slime-js)
+
+
 ; js2mode kinda freaks out with autopair, so just let js2 do its thing without
 ; autopair
 (add-hook 'js2-mode-hook
           '(lambda () (setq autopair-dont-activate t)))
+
+
+(global-set-key (kbd "M-r") 'slime-js-reload)
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (slime-js-minor-mode 1)))
+
+(add-hook 'css-mode-hook
+          (lambda ()
+            (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
+            (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
